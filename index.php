a86038d20b1e938be2b0d2c25001f409833ce2b9
Ivan Larikov <larikovivan49@gmail.com>
2024-12-06 07:13:17 -0500

    Disable dump to PDF

--- index.php
+++ index.php
@@ -1,44 +1,23 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Content-Type: text/plain");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
 use horstoeko\zugferd\ZugferdDocumentReader;
 use horstoeko\zugferdvisualizer\ZugferdVisualizer;
 
-require dirname(__FILE__) . "/vendor/autoload.php";
+require dirname(__FILE__) . '/vendor/autoload.php';
 
-$isCacheEnabled = true;
-$xml = file_get_contents("php://input");
-$fileKey = empty($_GET['id']) ? time() : $_GET['id'];
-$fileName = md5($fileKey . '---' . strlen($xml)) . ".pdf";
-$filePath = dirname(__FILE__) . "/generated/" . $fileName;
+$xml = file_get_contents('php://input');
 
-if (!$isCacheEnabled || !file_exists($filePath)) {
-	$document = ZugferdDocumentReader::readAndGuessFromContent($xml);
+$document = ZugferdDocumentReader::readAndGuessFromContent($xml);
 
-	$visualizer = new ZugferdVisualizer($document);
-	$visualizer->setDefaultTemplate();
-	$visualizer->setPdfFontDefault("courier");
-	$visualizer->renderPdfFile($filePath);
-}
+$visualizer = new ZugferdVisualizer($document);
+$visualizer->setDefaultTemplate();
+$visualizer->setPdfFontDefault('courier');
+// $visualizer->renderPdfFile($filePath);
+$pdfString = $visualizer->renderPdf();
 
-
-// Get Base URL
-$protocol = (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] !== "off" || $_SERVER["SERVER_PORT"] == 443) ? "https://" : "http://";
-$host = $_SERVER["HTTP_HOST"];
-$requestUri = $_SERVER["REQUEST_URI"];
-if (strpos($requestUri, "?") !== false) {
-	$requestUri = substr($requestUri, 0, strpos($requestUri, "?"));
-}
-$requestUrl = $protocol . $host . $requestUri;
-$patts = explode("/", $requestUrl);
-if ($patts[count($patts) - 1] == "index.php") {
-	array_pop($patts);
-}
-$baseUrl = implode("/", $patts);
-
-// Get PDF URL
-$pdfUrl = $baseUrl . "/generated/" . $fileName;
-
-echo $pdfUrl;
+header('Content-Type: application/pdf');
+header('Content-Disposition: inline; filename="e-invoice.pdf"');
+header('Content-Length: ' . strlen($pdfString));
+echo $pdfString;
